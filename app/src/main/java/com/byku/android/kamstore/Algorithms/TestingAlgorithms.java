72531dedbcde92a234baa91d0a376e31e63c1da6
byku - Przemysław Łybyk <prz.lyb@gmail.com>
2016-07-29 23:45:42 +0200

    Hello

---
+++ app/src/main/java/com/byku/android/kamstore/Algorithms/TestingAlgorithms.java
@@ -0,0 +1,92 @@
+package com.byku.android.kamstore.Algorithms;
+
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+
+/**
+ * Created by Byku on 28.07.2016.
+ */
+final public class TestingAlgorithms {
+    private TestingAlgorithms(){}
+    private static SparseIntArray ifCollapsed = new SparseIntArray();
+    private static SparseIntArray viewsDimensions = new SparseIntArray();
+    private static boolean isCollapsed;
+
+
+    public static void expand(final View v) {
+        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //final int targetHeight = v.getMeasuredHeight();
+        final int targetHeight = viewsDimensions.get(v.getId())<1 ? 1 : viewsDimensions.get(v.getId());
+        Log.i("LOG","Wys expand " + targetHeight);
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation()
+        {
+            /*@Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? ViewGroup.LayoutParams.WRAP_CONTENT
+                        : (int)(targetHeight * interpolatedTime);
+                Log.i("LOG","height: " + v.getLayoutParams().height);
+                v.requestLayout();
+            }//*/
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = targetHeight == 1 ? -2 : (int)(targetHeight * interpolatedTime);
+                Log.i("LOG","height: " + v.getLayoutParams().height + ", float" + interpolatedTime);
+                v.requestLayout();
+            }
+
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        //stores states of views
+        ifCollapsed.put(v.getId(),0);
+        // 1dp/ms
+        a.setDuration((int)(targetHeight*2 / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public static void collapse(final View v) {
+        viewsDimensions.put(v.getId(),v.getLayoutParams().height);
+        final int initialHeight = v.getMeasuredHeight();
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if(interpolatedTime == 1){
+                    v.setVisibility(View.GONE);
+                }else{
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+
+        //stores states of views
+        ifCollapsed.put(v.getId(),1);
+        // 1dp/ms
+        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    //implement exceptions!!
+    public static Integer getIfCollapsed(Integer id){
+        return ifCollapsed.get(id);
+    }
+}
