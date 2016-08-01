74e6d931cc8dd877b5c9226182f7725ab30652ce
byku - Przemysław Łybyk <prz.lyb@gmail.com>
2016-08-01 17:14:52 +0200

    Hello

--- app/src/main/java/com/byku/android/kamstore/RecView/ItemAdapter.java
+++ app/src/main/java/com/byku/android/kamstore/RecView/ItemAdapter.java
@@ -3,7 +3,6 @@ package com.byku.android.kamstore.RecView;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
